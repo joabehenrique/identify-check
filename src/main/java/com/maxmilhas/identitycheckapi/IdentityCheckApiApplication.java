c16ba83715a91db26391e21a760e6789fde567e0
joabehenrique <henriquecidoz@hotmail.com>
2023-04-04 23:28:31 -0300

    initial commit

---
+++ src/main/java/com/maxmilhas/identitycheckapi/IdentityCheckApiApplication.java
@@ -0,0 +1,13 @@
+package com.maxmilhas.identitycheckapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityCheckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityCheckApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxmilhas/identitycheckapi/IdentityCheckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxmilhas.identitycheckapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityCheckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
