407d4d8e644376dceb8445f2f094a7d8651d2137
joabehenrique <henriquecidoz@hotmail.com>
2023-04-06 00:08:05 -0300

    feat: added exception interceptor

---
+++ src/main/java/com/maxmilhas/identitycheckapi/exception/ControllerExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.maxmilhas.identitycheckapi.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(CpfNotFoundException.class)
+    public ResponseEntity<StandardError> CpfNotFound(CpfNotFoundException messageError, HttpServletRequest request) {
+
+        StandardError error = new StandardError(
+                Instant.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "Cpf not found",
+                messageError.getMessage(),
+                request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
